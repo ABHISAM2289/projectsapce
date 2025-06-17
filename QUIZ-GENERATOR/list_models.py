917f0aafb8819aa22f0a129fe4f0de5ee839ef09
abhishek samuel <141474796+ABHISAM2289@users.noreply.github.com>
2025-06-17 17:06:00 +0530

    Delete QUIZ-GENERATOR directory

--- QUIZ-GENERATOR/list_models.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-# Replace with your actual Gemini API key
-genai.configure(api_key="AIzaSyAjcs77CwCgqvGxZ4b_8TbBmS7bSGWtMYo")
-
-for model in genai.list_models():
-    print(model.name)
